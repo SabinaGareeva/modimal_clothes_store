1773b7c57d0068e7fac18165fe645388505c614a
Gareeva Sabina <gareevasaab@mail.ru>
2024-03-01 21:33:14 +0500

    added products to wishlist page

--- src/pages/Wishlist.jsx
+++ src/pages/Wishlist.jsx
@@ -1,12 +1,41 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useRouter } from "next/router";
+import Cards from "../components/cards/Cards";
 import Footer from "../components/layout/Footer/Footer";
 
-
 const Wishlist = () => {
+  // получение данных с сервера с whishlist
+  const [whishlistProducts, setWhishListProducts] = useState([]);
+  useEffect(() => {
+    const fetchDataWishlist = async () => {
+      try {
+        const response = await fetch("http://localhost:3000/wishlist");
+        const result = await response.json();
+        setWhishListProducts(result);
+      } catch {
+        console.log("Error", error);
+      }
+    };
+    fetchDataWishlist();
+  }, []);
+  // Получаю путь страницы для окрашивания иконки
+  const page=useRouter()
+  const currentPage =page.pathname
+  console.log(currentPage)
+
   return (
     <section className="collection">
+      <div className="mb-5">
       <div className="container">
-      Мой вишлист
+        <h2>My Wish List</h2>
+        <div
+          className="grid  grid-cols-3 gap-y-16 gap-x-24"
+          id="container-cards"
+        >
+          {whishlistProducts.map((element, index) => (
+            <Cards prodactElement={{ element, index }} key={index}></Cards>
+          ))}
+        </div></div>
       </div>
       <Footer></Footer>
     </section>
